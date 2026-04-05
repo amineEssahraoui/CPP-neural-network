8b27fadb5340b78eacc95fff0e7cdba574ef1b8e
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-05 17:00:02 +0100

    Add dot method for matrix multiplication

--- src/core/Matrix.cpp
+++ src/core/Matrix.cpp
@@ -13,3 +13,26 @@ double Matrix::at(int r, int c) const{
     int index = r*this->cols + c; 
     return this->data[index] ; 
 }
+
+Matrix Matrix::dot(const Matrix& other) {
+    // checking dim : 
+    if (this->cols != other.rows) {
+        cerr << "Error: Dimensions mismatch for dot product!" << endl ; 
+        return Matrix(0,0); 
+    }
+    int row_result = this->rows ; 
+    int n = this->cols ; 
+    int cols_result = other.cols ; 
+    Matrix Result(row_result, cols_result); 
+
+    for (int i=0 ; i<row_result ; i++) {
+        for (int j=0 ; j<cols_result ; j++) {
+            for (int k=0 ; k<n ; k++) {
+                Result.data[i*cols_result + j] += this->at(i,k)*other.at(k,j); 
+            }
+        }
+    }
+    return Result ; 
+}
+
+
