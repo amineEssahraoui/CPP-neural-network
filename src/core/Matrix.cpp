f3763abdcff1ec05c0f2ba52722a8cc4cfff013b
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-05 18:18:36 +0100

    Creat the main function to test a simple example

--- src/core/Matrix.cpp
+++ src/core/Matrix.cpp
@@ -66,3 +66,19 @@ void Matrix::print() const {
         std::cout << std::endl;
     }
 }
+
+
+int main () {
+    Matrix A(2,3); 
+    Matrix B(3,2); 
+    A.randomize(); B.randomize(); 
+    cout <<"A matrix :" << endl; A.print(); cout << endl; 
+    cout <<"B matrix :" << endl; B.print(); cout << endl;  
+
+    Matrix C = A.dot(B) ; 
+    cout <<"C matrix :" << endl; C.print(); cout << endl;
+    Matrix bias(2,2) ;
+    C.add(bias); 
+    cout <<"C matrix plus bias :" << endl; C.print(); cout << endl;
+    return 0; 
+}
