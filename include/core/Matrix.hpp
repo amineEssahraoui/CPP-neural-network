a1c4aebd4ad526ee08334e482334cd9e812cd4e9
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-05 15:48:00 +0100

    Creat the matrix class

---
+++ include/core/Matrix.hpp
@@ -0,0 +1,25 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+using namespace std ; 
+
+#include <vector> 
+#include <iostream>
+
+class Matrix {
+    public : 
+    int rows , cols; 
+    vector<double> data; // Flaty array
+
+    // Constructors : 
+    Matrix(int r , int c); 
+
+    // Core operations : 
+    Matrix dot(const Matrix& other); 
+    void add(const Matrix& other); 
+
+    // Getters , helpers : 
+    double at(int r , int c) const; 
+    void print() const; 
+} ; 
+
+#endif
