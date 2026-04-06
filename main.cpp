9808d8b38ac39639ce5f1337de9ad628605c6bd2
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-06 12:00:43 +0100

    Add test script for activation functions

--- main.cpp
+++ main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
 #include "core/Matrix.hpp"
+#include "Activation/Relu.hpp"
+#include "Activation/Sigmoid.hpp"
+#include "Activation/Tanh.hpp"
 
 using namespace std;
+using namespace Activation;
 
+int main() {
+    Matrix m(2, 3);
+    m.randomize();
+    
+    cout << "========== Matrix Originale ==========" << endl;
+    m.print();
+    cout << endl;
 
-int main () {
-    Matrix A(2,3); 
-    Matrix B(3,2); 
-    A.randomize(); B.randomize(); 
-    cout <<"A matrix :" << endl; A.print(); cout << endl; 
-    cout <<"B matrix :" << endl; B.print(); cout << endl;  
+    ReLU relu;
+    Matrix m_relu = relu.apply_matrix(m); 
+    Matrix d_relu = relu.derivative_matrix(m); 
+    
+    cout << "========== Test ReLU ==========" << endl;
+    cout << "Applied ReLU:" << endl;
+    m_relu.print();
+    cout << "Derivative ReLU:" << endl;
+    d_relu.print();
+    cout << endl;
 
-    Matrix C = A.dot(B) ; 
-    cout <<"C matrix :" << endl; C.print(); cout << endl;
-    Matrix bias(2,2) ;
-    bias.randomize(); 
-    cout <<"Bias matrix :" << endl; bias.print(); cout << endl; 
-    C.add(bias); 
-    cout <<"C matrix plus bias :" << endl; C.print(); cout << endl;
-    return 0; 
+    Sigmoid sigmoid;
+    Matrix m_sig = sigmoid.apply_matrix(m);
+    Matrix d_sig = sigmoid.derivative_matrix(m);
+    
+    cout << "========== Test Sigmoid ==========" << endl;
+    cout << "Applied Sigmoid:" << endl;
+    m_sig.print();
+    cout << "Derivative Sigmoid:" << endl;
+    d_sig.print();
+    cout << endl;
+
+    Tanh tanh_func;
+    Matrix m_tanh = tanh_func.apply_matrix(m);
+    Matrix d_tanh = tanh_func.derivative_matrix(m);
+    
+    cout << "========== Test Tanh ==========" << endl;
+    cout << "Applied Tanh:" << endl;
+    m_tanh.print();
+    cout << "Derivative Tanh:" << endl;
+    d_tanh.print();
+    cout << endl;
+
+    return 0;
 }
