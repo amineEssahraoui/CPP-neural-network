fd28c78fc4d314c251769e145247855b447628a9
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-06 11:00:19 +0100

    The core of dense layer

---
+++ include/layers/DenseLayer.hpp
@@ -0,0 +1,21 @@
+#ifndef DENSE_LAYER_HPP
+#define DENSE_LAYER_HPP
+
+#include "core/Matrix.hpp"
+#include "Activation/Activation.hpp"
+
+namespace Layers {
+    template <typename ActivationType> 
+    class DenseLayer {
+        public : 
+            Matrix weghits; 
+            Matrix bias; 
+            ActivationType activation; 
+
+            DenseLayer(int input_size, int output_size); // Constructor
+            Matrix forward(const Matrix& input) const;  // A simple function for froward propagation
+
+    };
+}
+
+#endif
