cd9e76d32b1557695844a69f546a93427673ef2e
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-06 03:12:32 +0100

    Delete std

--- include/Activation/Sigmoid.hpp
+++ include/Activation/Sigmoid.hpp
@@ -4,15 +4,13 @@
 #include "Activation.hpp"
 #include <cmath>
 
-using namespace std ; 
-
 namespace Activation {
     class Sigmoid : public ActivationFunction<Sigmoid> {
         public : 
-            double apply(double x) const{
-                return 1.0/(1+exp(-x)); 
+            double apply(double x) const {
+                return 1.0/(1+ std::exp(-x)); 
             }
-            double derivative(double x) const{
+            double derivative(double x) const {
                 double s = apply(x); 
                 return s * (1-s); 
             }
