5302a1bd77e1f72f66e2be3b50457d67584d2513
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-08 00:57:18 +0100

    Add MAS loss

---
+++ include/Loss/MAE.hpp
@@ -0,0 +1,39 @@
+#ifndef MAE_HPP
+#define MAE_HPP
+
+#include <cmath>
+#include "Loss.hpp"
+
+namespace Loss {
+    class MAE : public LossFunction {
+    public:
+
+        double calculate(const Matrix& predicted, const Matrix& target) const override {
+            double error = 0.0;
+            size_t size_data = predicted.data.size();
+
+            for (size_t i = 0; i < size_data; i++) {
+                error += abs(predicted.data[i] - target.data[i]);
+            }
+
+            return error / static_cast<double>(size_data);
+        }
+
+        Matrix derivative(const Matrix& predicted, const Matrix& target) const override {
+            Matrix grad(predicted.rows, predicted.cols); 
+            double n = static_cast<double>(grad.data.size());
+
+            for (size_t i = 0; i < grad.data.size(); i++) {
+                double diff = predicted.data[i] - target.data[i];
+                if (diff > 0) {
+                    grad.data[i] = 1.0/n ;  
+                } else {
+                    grad.data[i] = -1.0/n ;  
+                }
+            }
+            return grad;
+        }
+    };
+}
+
+#endif
