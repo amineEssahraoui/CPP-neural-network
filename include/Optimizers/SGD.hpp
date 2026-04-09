6b50d7de10af2c99fc64782eb30c76638b9caaba
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-04-09 19:12:06 +0100

    Fixe include problems

--- include/Optimizers/SGD.hpp
+++ include/Optimizers/SGD.hpp
@@ -1,10 +1,12 @@
-#ifdef SGD_HPP
+#ifndef SGD_HPP
 #define SGD_HPP
 
 #include "Optimizer.hpp"
+#include "core/Matrix.hpp"
 
 namespace Optimizers {
     class SGD : public Optimizer {
+    public: 
         SGD(double lr = 0.01); 
         void update(Matrix& weights, Matrix& bias, const Matrix& dW, const Matrix& db) override;
     }; 
